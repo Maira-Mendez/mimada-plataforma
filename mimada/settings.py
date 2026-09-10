10729f3798c3ad5fa79b9eba47d9403ebfd8c6d8
Maira Mendez <maimendez02@gmail.com>
2026-09-10 07:35:42 -0500

    intento arreglar el erro de git

--- mimada/settings.py
+++ mimada/settings.py
@@ -158,7 +158,7 @@ AUTH_USER_MODEL = 'usuarios.Usuario'
 
 EMAIL_BACKEND = 'anymail.backends.brevo.EmailBackend'
 ANYMAIL = {
-    'BREVO_API_KEY': config('BREVO_API_KEY'),
+    'BREVO_API_KEY': config('BREVO_API_KEY', default=''),
 }
 DEFAULT_FROM_EMAIL = 'Mimada 🌸 <mimada.detalles@gmail.com>'
 
